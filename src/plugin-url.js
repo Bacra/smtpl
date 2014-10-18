947ab597f0b1cc05bb5761d7d8fccf07f995b210
Bacra Woo <bacra.woo@gmail.com>
2014-10-19 00:01:39 +0800

    Fix: url plugin's parms is number or null

--- src/plugin-url.js
+++ src/plugin-url.js
@@ -1,12 +1,14 @@
 main.url = newSmtpl(function(_asName, _aoParams, _asTotal, _asTag, _aoReplaceArgs)
 {
-	var _sVal = _aoParams[_aoReplaceArgs[3]];
+	var _sName = _aoReplaceArgs[3];
 
-	if (_sVal)
+	if (_sName in _aoParams)
 	{
+		var _sVal = ''+_aoParams[_sName];
 		return _aoReplaceArgs[2] == '#' ? _sVal : encodeURIComponent(_sVal);
 	}
 
 	return '';
+
 }, '(#?)(\\w+?)');
 
